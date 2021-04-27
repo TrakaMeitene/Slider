a293ca09f7cceb414087fbfcbce26901f7666dfe
TrakaMeitene <sandra.jurberga@gmail.com>
2021-04-27 15:43:25 +0300

    js

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 var slideIndex = 1;
-showDivs(slideIndex);
+showSlides(slideIndex);
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+
 }
 
-function showDivs(n) {
+
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
   var i;
-  var x = document.getElementsByClassName("bilde");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  x[slideIndex-1].style.display = "block";  
+  var slides = document.getElementsByClassName("slider");
+
+
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+
 }
+  slides[slideIndex-1].style.display = "flex";
+
+} 
